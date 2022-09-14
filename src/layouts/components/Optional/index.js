fe1aafc0e4ee042d084719d3fdf0fca92343ec7b
phuocday <phuocday@gmail.com>
2022-09-14 13:44:16 +0700

    Coponent Optional

--- src/layouts/components/Optional/index.js
+++ src/layouts/components/Optional/index.js
@@ -1,8 +1,36 @@
+import { useState } from 'react';
 import style from './Optional.module.scss';
 import classNames from 'classnames/bind';
 
+import listOption from './listOption';
+
 let cx = classNames.bind(style);
 function Optional() {
-    return <div className={cx('wrapper')}></div>;
+    let [listOt, setListOt] = useState(listOption);
+    function handeleCheck(index) {
+        let newState = [...listOt];
+        for (let item in newState) {
+            if (newState[item].check == true) {
+                newState[item].check = false;
+            }
+        }
+        newState[index].check = true;
+        setListOt([...newState]);
+    }
+    return (
+        <div className={cx('wrapper')}>
+            {listOt.map((option, index) => {
+                return (
+                    <span
+                        onClick={() => handeleCheck(index)}
+                        key={index}
+                        className={cx('option', `${option.check ? 'active' : ''}`)}
+                    >
+                        {option.title}
+                    </span>
+                );
+            })}
+        </div>
+    );
 }
 export default Optional;
---
+++ src/layouts/components/Optional/listOption.js
@@ -0,0 +1,47 @@
+let listOption = [
+    {
+        check: true,
+        title: 'Tất cả',
+    },
+    {
+        check: false,
+        title: 'Âm nhạc',
+    },
+    {
+        check: false,
+        title: 'Trực tiếp',
+    },
+    {
+        check: false,
+        title: 'Danh sách kếp hợp',
+    },
+    {
+        check: false,
+        title: 'Trò chơi',
+    },
+    {
+        check: false,
+        title: 'Bóng đá',
+    },
+    {
+        check: false,
+        title: 'Nấu ăn',
+    },
+    {
+        check: false,
+        title: 'Hoạt hình',
+    },
+    {
+        check: false,
+        title: 'Mới tải lên gần đây',
+    },
+    {
+        check: false,
+        title: 'Đã xem',
+    },
+    {
+        check: false,
+        title: 'Đề xuất mới',
+    },
+];
+export default listOption;
